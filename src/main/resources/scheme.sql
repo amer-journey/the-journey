b0177ac702f59ccf870a09b2699cdfbb0a029697
Amer Alghfaili <ameralghfili@gmail.com>
2023-06-10 19:38:39 +0300

    add queries

--- src/main/resources/scheme.sql
+++ src/main/resources/scheme.sql
@@ -42,6 +42,27 @@ CREATE TABLE IF NOT EXISTS courses (
     FOREIGN KEY (parent_course_id) REFERENCES course(id)
 )
 
+CREATE TABLE IF NOT EXISTS students_courses (
+    student_id VARCHAR(50) NOT NULL,
+    course_id INT NOT NULL,
+    FOREIGN KEY (student_id) REFERENCES students(id)
+    FOREIGN KEY (course_id) REFERENCES courses(id)
+)
+
+CREATE TABLE IF NOT EXISTS instructors_courses (
+    instructor_id VARCHAR(50) NOT NULL,
+    course_id INT NOT NULL,
+    FOREIGN KEY (instructor_id) REFERENCES instructors(id)
+    FOREIGN KEY (course_id) REFERENCES courses(id)
+)
+
+CREATE TABLE IF NOT EXISTS instructors_course_contents (
+    instructor_id VARCHAR(50) NOT NULL,
+    course_content_id INT NOT NULL,
+    FOREIGN KEY (instructor_id) REFERENCES instructors(id)
+    FOREIGN KEY (course_content_id) REFERENCES course_contents(id)
+)
+
 CREATE TABLE IF NOT EXISTS reviews (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
@@ -85,9 +106,14 @@ CREATE TABLE IF NOT EXISTS announcements (
     title VARCHAR(50) NOT NULL,
     text VARCHAR(200),
     creation_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    course_content_id INT NOT NULL,
-    user_id VARCHAR(50) NOT NULL,
-    FOREIGN KEY (course_content_id) REFERENCES course_contents(id),
-    FOREIGN KEY (user_id) REFERENCES users(username)
-    FOREIGN KEY (parent_comment_id) REFERENCES comments(id)
+    course_id INT NOT NULL,
+    FOREIGN KEY (course_id) REFERENCES courses(id),
+)
+
+CREATE TABLE IF NOT EXISTS announcements_instructors (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    instructor_id VARCHAR(50) NOT NULL,
+    announcement_id INT NOT NULL,
+    FOREIGN KEY (instructor_id) REFERENCES instructors(id),
+    FOREIGN KEY (announcement_id) REFERENCES announcements(id),
 )
